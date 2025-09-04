84ff48452e0ee0c34798b163d642145eeb9e7dbf
Bintang Syahri <anjayanto08@gmail.com>
2025-09-04 08:51:12 +0700

    update

--- inter.js
+++ inter.js
@@ -1,7 +1,7 @@
     const starsContainer = document.querySelector(".stars");
     const colors = ["#cc00ff", "#6600ff", "#0033ff", "#00ccff", "#00ffcc"];
     
-    for (let i = 0; i < 30; i++) { 
+    for (let i = 0; i < 20; i++) { 
         const star = document.createElement("span");
         const size = Math.random() * 6 + 2; 
         star.style.width = size + "px";
@@ -13,9 +13,18 @@
         star.style.animationDelay = Math.random() * 5 + "s";
         starsContainer.appendChild(star);
       }
-  
+
     const menuToggle = document.getElementById("menu-toggle");
     const navbar = document.getElementById("navbar");
+    const menuIcon = document.querySelector("i");
+
     menuToggle.addEventListener("click", () => {
       navbar.classList.toggle("show");
+      if (navbar.classList.contains("show")) {
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+      } else {
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+      }
     });
